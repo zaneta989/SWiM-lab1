4f64986b83c77ae5fb5c59d2d9563727dd60aef6
zaneta989 <zaneta989@gmail.com>
2017-03-23 20:28:23 +0100

    zmiany w pliku aktywno 1

--- app/src/main/java/com/example/zaneta/mojaaplikacja/Aktywnosc2.java
+++ app/src/main/java/com/example/zaneta/mojaaplikacja/Aktywnosc2.java
@@ -2,6 +2,7 @@ package com.example.zaneta.mojaaplikacja;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Aktywnosc2 extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class Aktywnosc2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aktywnosc2);
+        View mojeOkno2 = findViewById(R.id.mojeOkno2);
+        mojeOkno2.setOnLongClickListener(new View.OnLongClickListener()
+        { public boolean onLongClick(View v)
+            {
+                        finish();
+                        return false;
+            }
+        } );
     }
 }

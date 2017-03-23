1f058eb4fbf863f1abd48cc6ff56983db2a59c59
zaneta989 <zaneta989@gmail.com>
2017-03-23 20:25:56 +0100

    zmiany w pliku java aktywnosc 1, wywoania drugiej aktywnoci

--- app/src/main/java/com/example/zaneta/mojaaplikacja/Aktywnosc1.java
+++ app/src/main/java/com/example/zaneta/mojaaplikacja/Aktywnosc1.java
@@ -1,7 +1,10 @@
 package com.example.zaneta.mojaaplikacja;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Aktywnosc1 extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Aktywnosc1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aktywnosc1);
+        final Intent intencja1 = new Intent(this,Aktywnosc2.class);
+        Button przycisk1 = (Button) findViewById(R.id.button1);
+        przycisk1.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View widok) { startActivity(intencja1); }
+        });
+    }
+    public void uruchomDwa(View view) {
+        final Intent intencja2 = new Intent(this,Aktywnosc2.class);
+        startActivity(intencja2);
     }
 }

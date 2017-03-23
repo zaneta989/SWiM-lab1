917a6399401c81b7e74b05c46af7323239d648c3
zaneta989 <zaneta989@gmail.com>
2017-03-23 20:40:19 +0100

    przycisk dla 3 aktywnosci

---
+++ app/src/main/java/com/example/zaneta/mojaaplikacja/Aktywnosc3.java
@@ -0,0 +1,13 @@
+package com.example.zaneta.mojaaplikacja;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Aktywnosc3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktywnosc3);
+    }
+}

353a5a4de84812c6cb2d1a2417f21bbb8e95e7d6
zaneta989 <zaneta989@gmail.com>
2017-03-23 19:40:04 +0100

    initial

---
+++ app/src/main/java/com/example/zaneta/mojaaplikacja/Aktywnosc1.java
@@ -0,0 +1,13 @@
+package com.example.zaneta.mojaaplikacja;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Aktywnosc1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktywnosc1);
+    }
+}
